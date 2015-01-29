7710e1adeadb0a422042f2d8ec64ab3df3c023d3
Matt Brender <mjbrender@gmail.com>
2015-01-28 20:49:07 -0500

    clean up text file and pass to mail

--- parse.rb
+++ parse.rb
@@ -3,7 +3,16 @@
 require 'mail'
 
 to_parse = ARGV[0]
+full_file = ""
 
 File.open(to_parse).each(sep="\r") do |line|
-    puts line
+    full_file << line
 end
+
+# puts "this is the file saved"
+# puts "----------------------"
+# puts full_file
+
+mail = Mail.read_from_string(full_file)
+
+puts mail.parts.length
