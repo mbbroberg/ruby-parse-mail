05d9ee198a0f5e85f5d790056954680bd7eab0cd
Matt Brender <mjbrender@gmail.com>
2015-05-23 12:25:39 -0400

    Cleaning up and prepping updates

--- parse_mbox.rb
+++ parse_mbox.rb
@@ -2,7 +2,6 @@
 
 # Usage: ./parse_mbox.rb mailing_list.mbox
 # The input file should be in Mbox http://www.qmail.org/man/man5/mbox.html format
-
 # For Mailman/pipermail type mailing lists, you can retrieve them (if you're a list member) at:
 # http://www.example.com/mailman/private/<listname>.mbox/<listname>.mbox
 
@@ -11,10 +10,13 @@ require 'mail'
 file_name = ARGV[0]
 senders = {}
 msg_count = 0
-process_limit_num = 10000000000  # Only parse the first N messages
-                                  # Default is set to 100 so you don't   
+process_limit_num = 1000  # Only parse the first N messages
+                          # Default is set to 100 so you don't   
+
+
 puts "Parsing #{file_name}..."
 
+## Testing note.
 if process_limit_num < 100000000000
     puts "Your process_limit_num is set to #{process_limit_num}."
     puts "It looks like you're in testing mode."
@@ -23,11 +25,20 @@ end
 
 File.open(file_name,"r:iso-8859-2").slice_before(/^From /).each do | lines |
   # Drop the first line since it's the mbox-specific 'From ...@... Sun Aug 09 23:07:58 2009'
-  message_text = lines.drop(1).join()
+  message_text = lines.join()
 
   # Parse the message text into a Mail object instance
   msg = Mail.new(message_text)
+  puts msg.subject
+
+  # if the msg.subject is `ss:` it covers RE: and SV: and is a reply
+  # msg.to 
+  # msg.from
+  # Parse them for Basho or not
+  #   >> mail.from[0].split("@")
+  #   => ["tarvid", "ls.net"]
   
+
   # Keep a histogram of senders
   from = msg.header['from'].to_s
   if senders.has_key? from
@@ -36,6 +47,7 @@ File.open(file_name,"r:iso-8859-2").slice_before(/^From /).each do | lines |
     senders[from] = 1 
   end
 
+  # tally total messages 
   msg_count += 1
   if msg_count >= process_limit_num
     break
@@ -46,5 +58,5 @@ puts "--------------------------------------------------------------------------
 puts "| Total messages: #{msg_count} from #{senders.size} distinct authors |"
 puts "-----------------------------------------------------------------------------"
 
-# Print out the senders and # of emails they sent, in ascending order
-puts senders.sort_by { |k,v| v }
+#### Print out the senders and # of emails they sent, in ascending order
+# puts senders.sort_by { |k,v| v }
