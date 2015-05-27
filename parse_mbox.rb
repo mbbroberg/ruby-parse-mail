f682a0b8568a4a999e9b5c1b81e452b61d7735a5
Matt Brender <mjbrender@gmail.com>
2015-05-26 20:06:42 -0400

    Updated merge and confirmed functionality

--- parse_mbox.rb
+++ parse_mbox.rb
@@ -20,7 +20,6 @@
 # => Do they answer questions? 
 ###############################################
 
-
 require 'mail'
 require 'csv'
 
@@ -30,31 +29,30 @@ if ARGV.size < 1
   exit 
 end
 
-
-mbox_file = ARGV[0]
+file_name = ARGV[0]
 format = ARGV[1]
-#output_file = ARGV[2]
 senders = {}
 msg_count = 0
+process_limit_num = 1000  # Only parse the first N messages
+                          # Default is set to 100 so you don't   
 
-##### Purely for testing #########
-process_limit_num = 1000         # Only parse the first N messages
-                                 # Recommend setting to 100 so you don't  have to wait a while 
 
-if process_limit_num < 100000000000 
+puts "Parsing #{file_name}..."
+
+## Testing note.
+if process_limit_num < 100000000000
     puts "Your process_limit_num is set to #{process_limit_num}."
     puts "It looks like you're in testing mode."
     puts "You may want to increase it to some absurd number to parse the whole file."
 end
 
-######## Parsing ###########
-
-File.open(mbox_file,"r:iso-8859-2").slice_before(/^From /).each do | lines |
+File.open(file_name,"r:iso-8859-2").slice_before(/^From /).each do | lines |
   # Drop the first line since it's the mbox-specific 'From ...@... Sun Aug 09 23:07:58 2009'
   message_text = lines.join()
 
   # Parse the message text into a Mail object instance
   msg = Mail.new(message_text)
+  puts msg.subject
 
   # Step 1:
   # msg.to 
@@ -73,15 +71,8 @@ File.open(mbox_file,"r:iso-8859-2").slice_before(/^From /).each do | lines |
   from = msg.header['from'].to_s
   if senders.has_key? from
     senders[from] += 1
-
-  id = msg.header['message-id'].to_s
-  sender = msg.header['from'].to_s
-
-  # Histogram of all users and the number of times we've seen them.
-  if senders.has_key? sender
-    senders[sender] += 1
   else
-    senders[sender] = 1
+    senders[from] = 1 
   end
 
   # Step 4: 
@@ -90,29 +81,28 @@ File.open(mbox_file,"r:iso-8859-2").slice_before(/^From /).each do | lines |
   if msg_count >= process_limit_num
     break
   end
-
-
-######## Output #################
+end
 
 puts "-----------------------------------------------------------------------------"
-puts "| Total messages: #{msg_count} sender #{senders.size} distinct authors |"
+puts "| Total messages: #{msg_count} from #{senders.size} distinct authors |"
 puts "-----------------------------------------------------------------------------"
 
 #### Print out the senders and # of emails they sent, in ascending order
 # puts senders.sort_by { |k,v| v }
+
 if format == "--format=csv"
-  column_names = senders.first
+  puts "Printing out to CSV as well."
   s=CSV.generate do |csv|
-    csv << ["Sender, ", "Tally", "Total Messages", "Total Senders"] # Name the columns
+    csv << ["Sender", "Tally", "Total Messages", "Total Senders"] # Name the columns
     csv << ["","",msg_count, senders.size]
     senders.each do |x|
       new_x = []
       if x[0]
         new_x[0] = x[0].sub('<', ', ').sub('>', '') # strip brackets in a safe way
-        new_x[0].gsub! /"/, ''                      # handle case where there are excess double quotes
-      end 
-      if x[1].class == String                       # If x[1] exists and it's a string, then we can normalize to down
-        new_x[1] = x[1].downcase
+        new_x[0].gsub!(/"/, '')
+      end
+      if x[1].class == String
+        new_x[1] = x[1].downcase                  # If x[1] exists and it's a string, then we can normalize to down
       end
       csv << new_x
     end
