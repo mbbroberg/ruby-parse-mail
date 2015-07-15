9c4ed4758f4a17ec655b673ceaa66da6fefde288
Matt Brender <mjbrender@gmail.com>
2015-07-15 15:34:29 -0700

    documenting how to pull mbox
    Crawling and direct mbox download now.

--- lazy-get.sh
+++ lazy-get.sh
@@ -1,2 +1,6 @@
+#### How to scrape for files as a user 
 # wget -r -l1 -H -t1 -nd -N -np -A.mp3 -erobots=off [url of website]
 # http://lists.basho.com/pipermail/riak-users_lists.basho.com/
+
+##### How I get it as an admin
+# wget --save-cookies cookies.txt --post-data "user=USER&password=PASSWORD" http://lists.basho.com/mailman/private/riak-users_lists.basho.com.mbox/riak-users_lists.basho.com.mbox
